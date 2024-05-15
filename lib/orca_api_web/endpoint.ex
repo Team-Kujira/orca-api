120db06892efac01df0e83c5410b30300af40874
codehans <94654388+codehans@users.noreply.github.com>
2024-05-15 14:40:17 +0100

    more cors

--- lib/orca_api_web/endpoint.ex
+++ lib/orca_api_web/endpoint.ex
@@ -44,5 +44,6 @@ defmodule OrcaApiWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug CORSPlug, headers: ["content-type"]
   plug OrcaApiWeb.Router
 end
